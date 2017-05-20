aee544c0c9f4eb87dcd4a05b6516349b6e47c348
Hernan Obera <hernan.obera@gmail.com>
2017-05-20 18:21:52 +0800

    implement Presenter for for TopStories create Contract between VIew and Presenter

---
+++ app/src/main/java/com/hobera/app/hnreader/topstories/TopStoriesContract.java
@@ -0,0 +1,29 @@
+package com.hobera.app.hnreader.topstories;
+
+import android.support.annotation.NonNull;
+
+import com.hobera.app.hnreader.BasePresenter;
+import com.hobera.app.hnreader.BaseView;
+import com.hobera.app.hnreader.data.Item;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Hernan Obera on 5/20/2017.
+ */
+/**
+ * This specifies the contract between the view and the presenter.
+ */
+public interface TopStoriesContract {
+    interface View extends BaseView<Presenter> {
+        void showTopStoryList(ArrayList<Item> itemList);
+
+        void showNoTopStoryList();
+    }
+
+    interface Presenter extends BasePresenter {
+        void onActivityResult(int requestCode, int resultCode);
+
+        void loadTopStories();
+    }
+}
---
+++ app/src/main/java/com/hobera/app/hnreader/topstories/TopStoriesPresenter.java
@@ -0,0 +1,22 @@
+package com.hobera.app.hnreader.topstories;
+
+/**
+ * Created by Hernan Obera on 5/20/2017.
+ */
+
+public class TopStoriesPresenter implements TopStoriesContract.Presenter {
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode) {
+
+    }
+
+    @Override
+    public void loadTopStories() {
+
+    }
+}
