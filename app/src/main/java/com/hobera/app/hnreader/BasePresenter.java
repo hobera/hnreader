0ccd55041d4ec2565e977b8aa69a6b6c6fd4e384
Hernan Obera <hernan.obera@gmail.com>
2017-05-20 18:15:51 +0800

    create BaseView and Presenter for MVP implementation

---
+++ app/src/main/java/com/hobera/app/hnreader/BasePresenter.java
@@ -0,0 +1,11 @@
+package com.hobera.app.hnreader;
+
+/**
+ * Created by Hernan Obera on 5/20/2017.
+ */
+
+public interface BasePresenter {
+
+    void start();
+
+}
---
+++ app/src/main/java/com/hobera/app/hnreader/BaseView.java
@@ -0,0 +1,10 @@
+package com.hobera.app.hnreader;
+
+/**
+ * Created by Hernan Obera on 5/20/2017.
+ */
+
+public interface BaseView<T> {
+
+    void setPresenter(T presenter);
+}
